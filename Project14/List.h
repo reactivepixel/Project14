5ff87b1f21cdc6edd7f85b0b2d11e7bcc42effc9
Jarret Olsen <JTOlsen@student.fullsail.edu>
2025-09-26 14:21:50 -0400

    Create List class

---
+++ Project14/List.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <vector>
+#include <string>
+#include "Task.h"
+
+class List
+{
+private:
+	std::vector<Task> m_Tasks;
+
+	std::string m_Name;
+
+public:
+
+	// methods
+};
+
